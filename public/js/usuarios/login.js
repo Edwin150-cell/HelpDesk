21a50a76389b95a69ea1609e27c5478247b069b4
Edwin150-cell <winalonso488@gmail.com>
2026-03-13 07:33:05 -0600

    Cambie las direcciones de mi carpeta

--- public/js/usuarios/login.js
+++ public/js/usuarios/login.js
@@ -1,18 +1,24 @@
 function loginUsuario(){
     $.ajax({
         type:"POST",
-        data:$('#frmLogin').serialize(),
+        data: $('#frmLogin').serialize(),
         url:"procesos/usuarios/login/loginUsuario.php",
         success:function(respuesta){
+
             respuesta = respuesta.trim();
-            if (respuesta == 1){
-                window.location.href = "vistas/inicio.php"
-            } else {
-                Swal.fire(":(","Error al entrar! " + respuesta, "error")
+
+            if(respuesta == 1){
+                window.location.href = "vistas/inicio.php";
+            }else{
+                Swal.fire({
+                    icon: "error",
+                    title: "Error",
+                    text: "Error al entrar! " + respuesta
+                });
             }
+
         }
     });
-
 
     return false;
 }
